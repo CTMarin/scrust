7a5b054028fe5254c66975d4ea2040a8cfcc5fc9
CTMarin <carlomaco11@gmail.com>
2022-01-16 01:40:31 +0100

    new(v0.2.0): added clap for cli app building

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ enum PortState {
 } 
 
 pub fn scan(ip: IpAddr, init_port: u16, end_port: u16) {
-    for port in init_port..end_port {
+    for port in init_port..=end_port {
         let result: PortState = port_connection(ip, port);
         match result {
             PortState::Open => println!("Port {} is open", port),
@@ -19,8 +19,7 @@ fn port_connection(ip: IpAddr, port: u16) -> PortState {
     let stream = TcpStream::connect((ip, port));
     match stream {
         Ok(_) => PortState::Open,
-        Err(error) => {
-            println!("{}", error);
+        Err(_) => {
             PortState::Closed
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use std::{net::IpAddr, str::FromStr};
-
+use clap::{Parser, IntoApp, ErrorKind};
 use scrust::scan;
+
+#[derive(Parser, Debug)]
+#[clap(author = "CTMarin", version = "0.1.0", about = "A simple port scanner written in Rust", long_about = None)]
+struct Scrust {
+    #[clap()]
+    address: String,
+
+    #[clap(short = 'i', long = "initial-port", default_value_t = 1)]
+    init_port: u16, 
+
+    #[clap(short = 'e', long = "end-port", default_value_t = 65535)]
+    end_port: u16
+}
 fn main() {
-    let ip = IpAddr::from_str("192.168.1.11").unwrap();
-    let init_port = 70;
-    let end_port = 85;
+    let args = Scrust::parse();
+    if args.init_port > args.end_port {
+        Scrust::into_app().error(
+            ErrorKind::InvalidSubcommand, 
+            "End port must be greater than initial port").exit();
+    }
+    let ip = IpAddr::from_str(&args.address).unwrap();
+    let init_port = args.init_port;
+    let end_port = args.end_port;
     scan(ip, init_port, end_port);        
 }
