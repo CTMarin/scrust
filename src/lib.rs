dfe84574054605bb0b5e3f8633d5f941730a0cc9
CTMarin <carlomaco11@gmail.com>
2022-01-16 02:07:07 +0100

    feat: added colorized output

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::net::{IpAddr, TcpStream};
+use colored::Colorize;
 
 enum PortState {
     Open,
@@ -6,13 +7,19 @@ enum PortState {
 } 
 
 pub fn scan(ip: IpAddr, init_port: u16, end_port: u16) {
+    let mut open_ports: Vec<u16> = Vec::new();
     for port in init_port..=end_port {
         let result: PortState = port_connection(ip, port);
         match result {
-            PortState::Open => println!("Port {} is open", port),
-            PortState::Closed => println!("Port {} is closed", port)
+            PortState::Open => {
+                open_ports.push(port);
+                println!("{}", format!("Port {} is open", port).green())
+            }
+            PortState::Closed => println!("{}", format!("Port {} is closed", port).red())
         }
     }
+    println!("------------- Open Ports -------------");
+    println!("{:?}", open_ports);
 }
 
 fn port_connection(ip: IpAddr, port: u16) -> PortState {
