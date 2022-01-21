6c1e5317e3cb63a2a610c4310070d5fd5b61dafc
CTMarin <carlomaco11@gmail.com>
2022-01-21 02:05:49 +0100

    feat: added colorize() method for PortState refactor: modularized port printing

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,37 @@ use tokio::{net::TcpStream};
 use tokio::time::{Instant, timeout_at};
 use std::time::Duration;
 use std::net::IpAddr;
-use colored::Colorize;
+use colored::{Colorize, ColoredString};
 use regex::{RegexBuilder};
 use std::fmt;
 
 enum PortState {
     Open,
     Closed,
-    Filtered
+    Filter
 } 
+
+impl PortState {
+    fn colorize(&self, output: String) -> ColoredString {
+        match *self {
+            PortState::Open => output.green(),
+            PortState::Closed => output.red(),
+            PortState::Filter => output.yellow(),
+        }
+    }
+}
 
 impl fmt::Display for PortState {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
             PortState::Open => write!(f, "open"),
             PortState::Closed => write!(f, "closed"),
-            PortState::Filtered => write!(f, "filtered")
+            PortState::Filter => write!(f, "filter")
         }
     }
 }
 
 pub async fn scan(ip: IpAddr, init_port: u16, end_port: u16) {
-    //let mut open_ports: Vec<u16> = Vec::new();
-    //let mut filtered_ports: Vec<u16> = Vec::new();
     let (sender, mut reciever) = tokio::sync::mpsc::channel(1024); 
     for port in init_port..=end_port {
         let sender_clone = sender.clone();
@@ -35,25 +43,25 @@ pub async fn scan(ip: IpAddr, init_port: u16, end_port: u16) {
     }
     
     std::mem::drop(sender);
-    let separator = "\t\t";
-    println!("{}", format!("Port{}State{}Service{}Version", separator, separator, separator).blue());
+    println!("{}", format!("Port\t\tState\t\tService\t\tVersion").blue());
     while let Some(value) = reciever.recv().await {
         match value {
-            // TODO: Separar los println! en una función aparte, aquí solo aplicar el color
-            (port, PortState::Open) => {
-                println!("{}", format!("{}\t\t{}\t\t{}", port, PortState::Open.to_string(), service(port).await).green());
+            (_, PortState::Closed) => (),
+            (port, state) => {
+                println!("{}", print_port(port, state).await);
             }
-            (port, PortState::Filtered) => {
-                println!("{}", format!("{}\t\t{}\t\t{}", port, PortState::Filtered.to_string(), service(port).await).yellow());
-            }
-            (_, _) => ()
-        }
-    }
+        };
+    };
+}
+
+async fn print_port(port: u16, state: PortState) -> ColoredString {
+    let port_info = format!("{}\t\t{}\t\t{}", port, state.to_string(), service(port).await);
+    state.colorize(port_info)
 }
 
 async fn port_connection(ip: IpAddr, port: u16) -> PortState {
     if let Err(_) = timeout_at(Instant::now() + Duration::from_millis(500), TcpStream::connect((ip, port))).await {
-        return PortState::Filtered;
+        return PortState::Filter;
     }
 
     let stream = TcpStream::connect((ip, port)).await;
@@ -76,7 +84,7 @@ async fn service(port: u16) -> String {
 #[cfg(target_family = "windows")]
 async fn service(port: u16) -> String {
     // C:\Windows\System32\drivers\etc on Windows
-    let content = tokio::fs::read("C:/Windows/System32/drivers/etc/services on Windows").await.unwrap();
+    let content = tokio::fs::read("C:/Windows/System32/drivers/etc/services").await.unwrap();
     let services = String::from_utf8(content).unwrap();
     let pattern = RegexBuilder::new(format!("([a-zA-Z-]+)(\\s*{}/tcp)", port).as_str());
     let re = pattern.build().unwrap();
